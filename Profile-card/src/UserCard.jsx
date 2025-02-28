72d68045074752df0fcbb57154b298a1ac3752f2
SaiDhinakar <saidhinakars899@gmail.com>
2025-03-01 00:36:18 +0530

    Profile card

--- Profile-card/src/UserCard.jsx
+++ Profile-card/src/UserCard.jsx
@@ -1,36 +1,75 @@
+import PropTypes from 'prop-types';
 
-function User() {
-    return (
-        <div className="card-container">
-            <span className="pro">ONLINE</span>
-            <img src="images/pic1.webp" className="img" alt="" />
-            <h3>Full Name</h3>
-            <h3>Tiruppur</h3>
-            <p>front-end developer</p>
-            <div className="buttons">
-                <button className="primary">Message</button>
-                <button className="primary outline">Following</button>
-            </div>
-            <div>
-                <h6 className="skills">Skills</h6>
-                <ul>
-                </ul>
-                    <li>UI / UX</li>
-                    <li>Front End Development</li>
-                    <li>HTML</li>
-                    <li>CSS</li>
-                    <li>JavaScript</li>
-                    <li>React</li>
-                    <li>Node</li>
-            </div>
-        </div>
-    )
+const UserData = [
+  {
+    name: "Jon Doe",
+    city: "New York",
+    description: "Front-end Developer",
+    skills: ["UI / UX", "HTML", "CSS", "JavaScritp", "React", "Node"],
+    online: true,
+    profile: "images/pic1.webp",
+  },
+  {
+    name: "Alice Bob",
+    city: "Quees",
+    description: "Cyber Security Expert",
+    skills: ["Networking", "Thread Management", "Network Management"],
+    online: false,
+    profile: "images/pic2.webp",
+  },
+];
+
+function User(props) {
+  return (
+    <div className="card-container">
+      <span className={props.online ? "pro online" : "pro offline"}>
+        {props.online ? "ONLINE" : "OFFLINE"}
+      </span>
+      <img src={props.profile} className="img" alt="" />
+      <h3>{props.name}</h3>
+      <h3>{props.city}</h3>
+      <p>{props.description}</p>
+      <div className="buttons">
+        <button className="primary">Message</button>
+        <button className="primary outline">Following</button>
+      </div>
+      <div className="skills">
+        <h6>Skills</h6>
+        <ul>
+          {props.skills.map((skill, index) => (
+            <li key={index}>{skill}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 const UserCard = () => {
   return (
-    <User />
-  )
-}
+    <>
+      {UserData.map((user, index) => (
+        <User
+          key={index}
+          name={user.name}
+          city={user.city}
+          description={user.description}
+          online={user.online}
+          profile={user.profile}
+          skills={user.skills}
+        />
+      ))}
+    </>
+  );
+};
 
-export default UserCard
+export default UserCard;
+
+User.propTypes = {
+  name: PropTypes.string.isRequired,
+  city: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  online: PropTypes.bool.isRequired,
+  profile: PropTypes.string.isRequired,
+  skills: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
